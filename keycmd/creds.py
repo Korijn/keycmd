e1e3bad7b93d17f6bc2fad2bdf975dfc3cd1a5ad
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2023-08-06 18:08:28 +0200

    Alias support

--- keycmd/creds.py
+++ keycmd/creds.py
@@ -14,6 +14,7 @@ def b64(value):
 def get_env(conf):
     """Load credentials from the OS keyring according to user configuration"""
     env = environ.copy()
+    loaded = {}
     for key, src in conf["keys"].items():
         password = keyring.get_password(src["credential"], src["username"])
         if password is None:
@@ -22,12 +23,24 @@ def get_env(conf):
                 f" with user {src['username']}"
                 f" as it does not exist"
             )
+        loaded[key] = password
         if src.get("b64"):
             password = b64(password)
         env[key] = password
         vlog(
             f"exposing credential {src['credential']}"
             f" with user {src['username']}"
+            f" as environment variable {key} (b64: {src.get('b64', False)})"
+        )
+    for key, src in conf.get("aliases", {}).items():
+        password = loaded.get(src["key"]])
+        if password is None:
+            error(f"MISSING alias key {src['key']}")
+        if src.get("b64"):
+            password = b64(password)
+        env[key] = password
+        vlog(
+            f"aliasing {src['key']}"
             f" as environment variable {key} (b64: {src.get('b64', False)})"
         )
     return env
